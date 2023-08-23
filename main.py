7b15cf0f928e806c351820966e17530da451eadd
Yoniher Melendez <yonihermelendez@gmail.com>
2023-08-23 12:35:34 -0500

    Se hace uso del metodo get dentro del proyecto

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+movies = [{
+    "id":1,
+    "Titulo": "hombres de negro",
+    "año": 2021
+},
+{
+    "id":2,
+    "Titulo": "avion gris",
+    "año": 2021
+}]
+
 
 app = FastAPI()
 app.title = "API - YTA Style"
@@ -6,4 +19,9 @@ app.version = "1.0"
 
 @app.get("/", tags=['home'])
 def message():
-    return "Hello World"
+    return HTMLResponse('<h1>Hola Mundo</h1>')
+
+
+@app.get("/movies", tags=['movies'])
+def get_movies():
+    return movies
