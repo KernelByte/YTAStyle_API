bfeeec754dacf96f65de2938e399dee35a5b7feb
Yoniher Melendez <yonihermelendez@gmail.com>
2023-08-24 16:51:59 -0500

    Se añade esquema para mejorar el envio de parametros

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional
+
 
 movies = [{
     "id":1,
@@ -17,6 +20,13 @@ app = FastAPI()
 app.title = "API - YTA Style"
 app.version = "1.0"
 
+
+class Movie(BaseModel):
+    id:int 
+    title: str
+    description: Optional[str] = 'Descripcion'
+
+
 @app.get("/", tags=['home'])
 def message():
     return HTMLResponse('<h1>Hola Mundo</h1>')
@@ -25,3 +35,11 @@ def message():
 @app.get("/movies", tags=['movies'])
 def get_movies():
     return movies
+
+@app.post('/movies',tags=['movies'])
+def crear(movie: Movie):
+        movies.append({
+            "id":3,
+            "Titulo": "Tiburon",
+            "año": 2023
+        })
