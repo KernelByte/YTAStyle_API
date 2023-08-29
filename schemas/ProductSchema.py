1c325789b1ce637f22c9cb5da1a14a70549c21c2
Yoniher Melendez <yonihermelendez@gmail.com>
2023-08-29 17:12:18 -0500

    Se crea esquema de producto

---
+++ schemas/ProductSchema.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import date
+
+class Product(BaseModel):
+      idProduct: Optional[int] = None
+      nameProduct: str
+      dateProduct: date
+      quantity: int
+      priceCost: float
+      priceBuy:  float
+      idCategoryProduct: int
+      idStatusProduct: int
+      color: str
+      description: str
+      barcode: str
